065cc558cf1e46eb29020af9094a08da21adadda
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2019-02-25 12:37:34 -0700

    lectura de archivos

--- PlantillaOpenGL/Shader.h
+++ PlantillaOpenGL/Shader.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Shader {
+public:
+	GLuint getID(); //obtiene el ID del shader
+	Shader(const char * rutaVertex, const char * rutaFragment);
+	void enlazar();
+	void desenlazar();
+
+private:
+	GLuint shaderID;
+	void verificarCompilacion(GLuint id);
+	void verificarVinculacion(GLuint id);
+};
