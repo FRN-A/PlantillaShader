8e16b08a2d572bb6d35ccf4a60d969c8ebef834d
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2019-02-27 09:47:25 -0700

    cargar y compilar shader

--- PlantillaOpenGL/Shader.cpp
+++ PlantillaOpenGL/Shader.cpp
@@ -6,6 +6,8 @@ GLuint Shader::getID() {
 }
 
 Shader::Shader(const char * rutaVertex, const char * rutaFragment) {
+
+	//Guardar en variables  el texto del codigo del shader
 	string codigoVertexShader;
 	ifstream vertexShaderStream(rutaVertex, ios::in);
 	if (vertexShaderStream.is_open()) {
@@ -18,4 +20,41 @@ Shader::Shader(const char * rutaVertex, const char * rutaFragment) {
 	else {
 		cout << "No se pudo abrir el archivo " << rutaVertex;
 	}
+
+	string codigoFragmentShader;
+	ifstream fragmentShaderStream(rutaFragment, ios::in);
+	if (fragmentShaderStream.is_open()) {
+		string linea;
+		while (getline(fragmentShaderStream, linea)) {
+			codigoFragmentShader += linea + "\n";
+		}
+		fragmentShaderStream.close();
+	}
+	else {
+		cout << "No se puede abrir el archivo: " << rutaFragment << endl;
+	}
+
+	//Convertir de string a cadena de char
+	const char*  cadenaCodigoVertex = codigoVertexShader.c_str();
+	const char* cadenaCodigoFragment = codigoFragmentShader.c_str();
+
+	//1.- Crear el programa de shader
+	shaderID = glCreateProgram();
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//2.- Cargar del codigo del shader
+	glShaderSource(vertexShaderID, 1, &cadenaCodigoVertex, NULL);
+	glShaderSource(fragmentShaderID, 1, &cadenaCodigoFragment, NULL);
+
+
+	//3.- Compilar los shaders
+	glCompileShader(vertexShaderID);
+	glCompileShader(fragmentShaderID);
+
+}
+
+void Shader::verificarCompilacion(GLuint id){
+	GLint resultado = GL_FALSE;
+	int longitudLog = 0;
 }
